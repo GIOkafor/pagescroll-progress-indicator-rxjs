3d5ba179cf65824c9608086bf654952325ac5510
GIOkafor <GIOkafor@users.noreply.github.com>
2022-12-06 12:36:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pagescroll-progress-indicator-rxjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-1nkc2q)
---
+++ index.ts
@@ -0,0 +1,23 @@
+import './style.css';
+
+import { map, fromEvent } from 'rxjs';
+
+//elements
+const progressBar: any = document.querySelector('.progress-bar');
+//helpers
+function calculateScrollPercent(element) {
+  const { scrollTop, scrollHeight, clientHeight } = element;
+
+  return (scrollTop / (scrollHeight - clientHeight)) * 100;
+}
+
+//streams
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+  map(({ target }: any) => calculateScrollPercent(target.documentElement))
+);
+
+progress$.subscribe((percent) => {
+  console.log({ percent });
+  progressBar.style.width = `${percent}%`;
+});
